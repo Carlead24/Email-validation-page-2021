252348f3208b70e7f0ad3542203f3650c9431d05
Carlead24 <79675296+Carlead24@users.noreply.github.com>
2021-11-09 02:12:28 +0100

    initial commit

---
+++ test.js
@@ -0,0 +1,23 @@
+// const person = {
+//     firstName:'khaleed',
+//     lastName: 'Awaiye',
+//     age: 28,
+//     hobbies: ['music', 'movies', 'sports'],
+//     address: {
+//         street:'42 odo street',
+//         city:'obalende',
+//         state:'lagos' }
+// }
+
+// console.log(person.firstName);
+
+
+// function addNums(num1, num2){
+//     console.log(num1+num2);
+// }
+// addNums(5,4);
+
+// function addNums(num1=1, num2=1){
+//     console.log(num1+num2);
+// }
+//     addNums(5,7);
